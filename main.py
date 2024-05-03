f7dc94e6bfd541cc4b3a321c612cc8bf7c6ea292
GutoFrr <gustavodelferreira.12@gmail.com>
2024-05-03 09:48:43 -0300

    feat: completed json integration

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ from os import system
 import json
 
 
+lists = {
+    "students": [],
+    "disciplines": [],
+    "teachers": [],
+    "classes": [],
+    "enrollments": [],
+}
+
+
 class MenuItem:
-    def __init__(self, id, name, active, empty_message):
+    def __init__(self, id, name, label, active, empty_message):
         self.id = id
         self.name = name
+        self.label = label
         self.active = active
         self.list = []
         self.empty_message = empty_message
 
     def add_item_to_list(self, new_item):
         self.list.append(new_item)
-        self.save_in_json()
 
     def display_operation_list(self):
-        self.get_from_json()
+        self.get_json_list()
         if len(self.list) == 0:
             print(self.empty_message)
         for item in self.list:
@@ -23,10 +32,9 @@ class MenuItem:
 
     def remove_item(self, item):
         self.list.remove(item)
-        self.save_in_json()
 
     def get_list_item(self, code):
-        if code.isnumeric():
+        if str(code).isnumeric():
             return next(filter(lambda item: item['codigo'] == int(code), self.list), None)
 
         else:
@@ -35,27 +43,19 @@ class MenuItem:
 
     def save_in_json(self):
         with open('data.json', 'w') as file:
-            json.dump({ self.name: self.list }, file)
+            json.dump({self.name: self.list}, file)
 
-    def get_from_json(self):
-        with open('data.json', 'r') as file:
-            self.list = json.load(file)[self.name]
-
-
-
-students = []
-disciplines = []
-teachers = []
-classes = []
-enrollments = []
+    def get_json_list(self):
+        self.list = lists[self.name]
 
 
 class Student(MenuItem):
     def __init__(self):
         self.id = "1"
-        self.name = "Estudante"
+        self.name = "students"
+        self.label = "Estudante"
         self.active = True
-        self.list = students
+        self.list = lists["students"]
         self.empty_message = "Não há estudantes cadastrados..."
 
     def add_student(self):
@@ -117,71 +117,82 @@ class Student(MenuItem):
         elif operation == "4":
             self.edit_student()
 
+        self.save_in_json()
+
 
 class Discipline(MenuItem):
     def __init__(self):
         self.id = "2"
-        self.name = "Disciplina"
+        self.name = "disciplines"
+        self.label = "Disciplina"
         self.active = False
-        self.list = disciplines
+        self.list = lists["disciplines"]
         self.empty_message = "Não há disciplinas cadastradas..."
 
 
 class Teacher(MenuItem):
     def __init__(self):
         self.id = "3"
-        self.name = "Professor"
+        self.name = "teachers"
+        self.label = "Professor"
         self.active = False
-        self.list = teachers
+        self.list = lists["teachers"]
         self.empty_message = "Não há professores cadastrados..."
 
 
 class Class(MenuItem):
     def __init__(self):
         self.id = "4"
-        self.name = "Turma"
+        self.name = "classes"
+        self.label = "Turma"
         self.active = False
-        self.list = classes
+        self.list = lists["classes"]
         self.empty_message = "Não há turmas cadastradas..."
 
 
 class Enrollment(MenuItem):
     def __init__(self):
         self.id = "5"
-        self.name = "Matrícula"
+        self.name = "enrollments"
+        self.label = "Matrícula"
         self.active = False
-        self.list = enrollments
+        self.list = lists["enrollments"]
         self.empty_message = "Não há matrículas cadastradas..."
 
 
 class Logout(MenuItem):
     def __init__(self):
         self.id = "6"
-        self.name = "Sair"
+        self.name = "logout"
+        self.label = "Sair"
         self.active = True
+        self.list = []
+        self.empty_message = ""
 
 
 menu = [Student(), Discipline(), Teacher(), Class(), Enrollment(), Logout()]
 
 
 class OperationItem:
-    def __init__(self, id, name, active):
+    def __init__(self, id, name, label, active):
         self.id = id
         self.name = name
+        self.label = label
         self.active = active
 
 
 operations_menu = [
-    OperationItem("1", "Incluir", True),
-    OperationItem("2", "Listar", True),
-    OperationItem("3", "Excluir", True),
-    OperationItem("4", "Alterar", True),
-    OperationItem("5", "Voltar ao menu principal", True),
+    OperationItem("1", "add", "Incluir", True),
+    OperationItem("2", "list", "Listar", True),
+    OperationItem("3", "delete", "Excluir", True),
+    OperationItem("4", "update", "Alterar", True),
+    OperationItem("5", "logout", "Voltar ao menu principal", True),
 ]
 
 
 def main():
     system("cls")
+    load_json_data()
     while True:
         display_menu(menu, "Bem-vindo!")
         menu_input = input("Selecione a área que deseja acessar: ")
@@ -202,7 +213,7 @@ def display_menu(menu_list, message):
     print(message)
     print()
     for item in menu_list:
-        print(f"{item.id}. {item.name}")
+        print(f"{item.id}. {item.label}")
     print()
 
 
@@ -217,18 +228,28 @@ def press_enter_to_stop():
     system("cls")
 
 
+def load_json_data():
+    try:
+        with open('data.json', 'r') as file:
+            json_data = json.load(file)
+            for key, value in dict(json_data).items():
+                lists[key] = value
+    except FileNotFoundError:
+        return
+
+
 def display_operations(menu_input):
     system("cls")
     while True:
         selected_menu = get_selected_menu_item(menu_input, menu)
 
         if bool(selected_menu.active) is False:
-            print(f'"{selected_menu.name}" está em desenvolvimento!')
+            print(f'"{selected_menu.label}" está em desenvolvimento!')
             print()
             break
 
         display_menu(operations_menu,
-                     f"***** Menu de operações - {selected_menu.name} *****")
+                     f"***** Menu de operações - {selected_menu.label} *****")
 
         operation_input = input("Selecione uma operação: ")
 
@@ -242,32 +263,29 @@ def display_operations(menu_input):
 
             while True:
                 if bool(selected_operation.active is False):
-                    print(f'"{selected_operation.name}" está em desenvolvimento!')
+                    print(
+                        f'"{selected_operation.label}" está em desenvolvimento!')
                     print()
                     break
+
+                print(f"{selected_menu.label} - {selected_operation.label}")
+                print()
 
                 if selected_operation.id == "2":
                     selected_menu.display_operation_list()
 
-                    press_enter_to_stop()
-                    break
-
-                if selected_menu.name == "Estudante":
-                    student = Student()
-                    while True:
-                        print(f"{selected_operation.name}")
-                        print()
+                else:
+                    if selected_menu.label == "Estudante":
+                        student = Student()
 
                         student.trigger_operation(selected_operation.id)
 
-                        break
-
-                    press_enter_to_stop()
-                    break
+                press_enter_to_stop()
+                break
 
         else:
             system("cls")
-            print("Opção inválida! Tente novamente.")
+            print("Opção inválida! Tente novamenteeee.")
             print()
 
 
